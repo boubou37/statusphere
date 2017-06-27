1bf470b58880bd6f3c7b8ae9952dbeeefb7385a8
boubou37 <lrahamefy@sfr.fr>
2017-06-28 01:23:32 +0200

    updated package.json

--- server/routes/api.js
+++ server/routes/api.js
@@ -5,25 +5,11 @@ mongoose.connect('mongodb://localhost/myappdatabase');
 
 
 // declare axios for making http requests
-const axios = require('axios');
 const API = 'https://jsonplaceholder.typicode.com';
 
 /* GET api listing. */
 router.get('/', (req, res) => {
   res.send('api works');
-});
-// Get all posts
-router.get('/posts', (req, res) => {
-  // Get posts from the mock api
-  // This should ideally be replaced with a service that connects to MongoDB
-  axios.get(`${API}/posts`)
-    .then(posts => {
-      res.status(200).json(posts.data);
-      console.log('get on posts');
-    })
-    .catch(error => {
-      res.status(500).send(error)
-    });
 });
 
 module.exports = router;
